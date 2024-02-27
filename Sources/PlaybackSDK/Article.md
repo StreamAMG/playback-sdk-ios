92d3bec78ce8d69c0490965447fcf6308bd2eec3
Franco Driansetti <7194678+francodriansetti@users.noreply.github.com>
2024-02-27 19:04:17 +0100

    Added interactive documentation

--- Sources/PlaybackSDK/Article.md
+++ Sources/PlaybackSDK/Article.md
@@ -1,15 +1,5 @@
 # Folder structure
 
-<!--@START_MENU_TOKEN@-->Summary<!--@END_MENU_TOKEN@-->
-
-## Overview
-
-<!--@START_MENU_TOKEN@-->Text<!--@END_MENU_TOKEN@-->
-
-### Section header
-
-<!--@START_MENU_TOKEN@-->Text<!--@END_MENU_TOKEN@-->
-
 PlaybackSDK/
 ├── PlayBackAPI/
 │   ├── PlayBackAPI.h
---
+++ Sources/PlaybackSDK/Documentation.docc/Tutorial/LoadHlsStreamTutorial.swift
@@ -0,0 +1,13 @@
+//
+//  LoadHlsStreamTutorial.swift
+//  
+//
+//  Created by Franco Driansetti on 27/02/2024.
+//
+
+import Foundation
+
+let entryId = "YOUR_ENTRY_ID"
+let authorizationToken = "YOUR_AUTHORIZATION_TOKEN" // optional
+
+PlaybackUIView(entryId: entryId, authorizationToken: authorizationToken)
