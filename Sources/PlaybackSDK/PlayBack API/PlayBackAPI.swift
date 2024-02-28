b4c5318e4221a22e286ce74d6d3e3d86361f856f
Franco Driansetti <7194678+francodriansetti@users.noreply.github.com>
2024-02-28 11:41:17 +0100

    Documentation and error codes
    Updated error codes
    updated documentation

--- Sources/PlaybackSDK/PlayBack API/PlayBackAPI.swift
+++ Sources/PlaybackSDK/PlayBack API/PlayBackAPI.swift
@@ -9,21 +9,6 @@ import Foundation
 import Combine
 
 /**
- Define the errors that can occur during API interactions
- */
-public enum PlayBackAPIError: Error {
-    
-    case invalidResponsePlaybackData
-    case invalidPlaybackDataURL
-    case invalidPlayerInformationURL
-    case initializationError
-    case loadHLSStreamError
-
-    case networkError(Error)
-    case apiError(statusCode: Int, message: String)
-}
-
-/**
  Protocol defining the methods required to interact with the Playback API.
  */
 internal protocol PlayBackAPI {
