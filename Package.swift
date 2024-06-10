77b1a7b008c0f24826abfabccb7627a9db141074
Stefano Russello <stefano.russello@gmail.com>
2024-06-10 11:23:31 +0200

    ZEUS-4346 Privacy Manifest Info
    Place resources section after dependencies (thanks to @artem-y-pamediagroup )

--- Package.swift
+++ Package.swift
@@ -35,11 +35,11 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "PlaybackSDK",
-            resources: [
-                .process("PrivacyInfo.xcprivacy")],
             dependencies: [
                 .product(name: "BitmovinPlayer", package: "BitmovinPlayer"),
-            ]
+            ],
+            resources: [
+                .process("PrivacyInfo.xcprivacy")]
         ),
         .testTarget(
             name: "PlaybackSDKTests",
