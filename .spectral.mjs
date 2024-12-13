79b6d85f95ef83e66b46b6e4a9f60fb32f2db787
Stefano Russello <stefano.russello@streamamg.com>
2024-12-13 11:05:07 +0100

    CORE-5261 Stoplight documentation
    - Removing spectral file
    - Adding spectral file to gitignore

--- .spectral.mjs
+++
@@ -1,2 +0,0 @@
-import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI2Njk0Nw/spectral.js?branch_slug=main&token=6f9a4117-1679-495a-b733-de6471675710";
-export default { extends: ruleset };
